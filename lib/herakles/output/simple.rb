5bf93d6491e866a33e8352a2afd2c70922280e01
Sean Redmond <github-smr@sneakemail.com>
2014-04-09 11:50:46 -0400

    Configure included css files
    Allow included css to be configured in the YAML file

--- lib/herakles/output/simple.rb
+++ lib/herakles/output/simple.rb
@@ -6,8 +6,19 @@ module Herakles
       def initialize(header_style, files)
         @header_style = header_style
         @files = files
+        @css_includes = css_includes(@files)
         @tpl_dir = File.join(File.dirname(__FILE__), 'templates')
         @html_t = self.load_erb(@tpl_dir, 'simple-html.erb')
+      end
+
+      def css_includes(files)
+        if files[:css_include].nil?
+          return nil
+        end
+
+        files[:css_include].map{|css|
+          %Q|<link rel="stylesheet" href="#{css}">|
+        }.join("\n")
       end
 
       def render
